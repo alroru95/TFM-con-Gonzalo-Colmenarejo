03868fdbf4a3931af1e5eb4ebe56e5d5953fc337
A. Rodríguez <66773906+alroru95@users.noreply.github.com>
2025-04-09 01:29:44 +0200

    Update and rename 2) Generación variantes protonadas metabolitos to 2) Generación variantes protonadas metabolitos.sh

--- Programación/2) Generación variantes protonadas metabolitos.sh
+++ Programación/2) Generación variantes protonadas metabolitos.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 python3 gypsum_dl/run_gypsum_dl.py -s TFM/Metabolitos/Metabolitos.sdf -o TFM/Metabolitos/ --num_processors -1 -m 5 --min_ph 6.4 --max_ph 8.5 --pka_precision 1.0 -t 7
 
 
