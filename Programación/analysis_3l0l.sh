512200f716d383a6fd7fc7b028b75eb2138aff44
A. Rodríguez <66773906+alroru95@users.noreply.github.com>
2025-04-09 02:01:17 +0200

    Add files via upload

---
+++ Programación/analysis_3l0l.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+cd TFM/dockings/metabolites/3l0l/mol2/
+
+mv Mol1/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol1/
+mv Mol1/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol1/
+mv Mol1/3l0l_Mol1_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol1/
+mv Mol1/3l0l_Mol1_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol1/
+
+mv Mol2/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol2/
+mv Mol2/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol2/
+mv Mol2/3l0l_Mol2_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol2/
+mv Mol2/3l0l_Mol2_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol2/
+
+mv Mol4/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol4/
+mv Mol4/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol4/
+mv Mol4/3l0l_Mol4_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol4/
+mv Mol4/3l0l_Mol4_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol4/
+
+mv Mol6/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol6/
+mv Mol6/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol6/
+mv Mol6/3l0l_Mol6_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol6/
+mv Mol6/3l0l_Mol6_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol6/
+
+mv Mol7/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol7/
+mv Mol7/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol7/
+mv Mol7/3l0l_Mol7_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol7/
+mv Mol7/3l0l_Mol7_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol7/
+
+mv Mol8/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol8/
+mv Mol8/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol8/
+mv Mol8/3l0l_Mol8_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol8/
+mv Mol8/3l0l_Mol8_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol8/
+
+mv Mol9/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol9/
+mv Mol9/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol9/
+mv Mol9/3l0l_Mol9_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol9/
+mv Mol9/3l0l_Mol9_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol9/
+
+mv Mol10/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol10/
+mv Mol10/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol10/
+mv Mol10/3l0l_Mol10_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol10/
+mv Mol10/3l0l_Mol10_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol10/
+
+mv Mol12/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol12/
+mv Mol12/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol12/
+mv Mol12/3l0l_Mol12_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol12/
+mv Mol12/3l0l_Mol12_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol12/
+
+mv Mol13/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol13/
+mv Mol13/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol13/
+mv Mol13/3l0l_Mol13_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol13/
+mv Mol13/3l0l_Mol13_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol13/
+
+mv Mol14/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol14/
+mv Mol14/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol14/
+mv Mol14/3l0l_Mol14_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol14/
+mv Mol14/3l0l_Mol14_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol14/
+
+mv Mol15/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol15/
+mv Mol15/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol15/
+mv Mol15/3l0l_Mol15_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol15/
+mv Mol15/3l0l_Mol15_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol15/
+
+mv Mol16/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol16/
+mv Mol16/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol16/
+mv Mol16/3l0l_Mol16_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol16/
+mv Mol16/3l0l_Mol16_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol16/
+
+mv Mol17/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol17/
+mv Mol17/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol17/
+mv Mol17/3l0l_Mol17_solv.pdb /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol17/
+mv Mol17/3l0l_Mol17_solv.prmtop /home/alonso/TFM/dockings/metabolites/3l0l/moldinamics/3l0l_Mol17/
+
+
+cd ..
+cd moldinamics
+
+cd 3l0l_Mol1
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol1_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol1_final.pdb
+plip -f 3l0l_Mol1_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 3l0l_Mol2
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol2_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol2_final.pdb
+plip -f 3l0l_Mol2_final.pdb
+cpptraj -i rmsd.in  
+
+cd ..
+
+cd 3l0l_Mol4
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol4_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol4_final.pdb
+plip -f 3l0l_Mol4_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 3l0l_Mol6
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol6_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol6_final.pdb
+plip -f 3l0l_Mol6_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 3l0l_Mol7
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol7_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol7_final.pdb
+plip -f 3l0l_Mol7_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 3l0l_Mol8
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol8_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol8_final.pdb
+plip -f 3l0l_Mol8_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 3l0l_Mol9
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol9_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol9_final.pdb
+plip -f 3l0l_Mol9_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 3l0l_Mol10
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol10_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol10_final.pdb
+plip -f 3l0l_Mol10_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 3l0l_Mol12
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol12_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol12_final.pdb
+plip -f 3l0l_Mol12_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 3l0l_Mol13
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol13_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol13_final.pdb
+plip -f 3l0l_Mol13_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 3l0l_Mol14
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol14_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol14_final.pdb
+plip -f 3l0l_Mol14_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 3l0l_Mol15
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol15_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol15_final.pdb
+plip -f 3l0l_Mol15_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 3l0l_Mol16
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol16_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol16_final.pdb
+plip -f 3l0l_Mol16_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 3l0l_Mol17
+cpptraj -i all_analysis.in
+ambpdb -p 3l0l_Mol17_solv.prmtop -c 10.prod_4.rst7 > 3l0l_Mol17_final.pdb
+plip -f 3l0l_Mol17_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+
+
+
+
+
+
+
+
---
+++ Programación/analysis_5m96.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+cd TFM/dockings/metabolites/5m96/mol2/
+
+mv Mol1/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol1/
+mv Mol1/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol1/
+mv Mol1/5m96_Mol1_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol1/
+mv Mol1/5m96_Mol1_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol1/
+
+mv Mol2/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol2/
+mv Mol2/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol2/
+mv Mol2/5m96_Mol2_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol2/
+mv Mol2/5m96_Mol2_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol2/
+
+mv Mol3/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol3/
+mv Mol3/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol3/
+mv Mol3/5m96_Mol3_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol3/
+mv Mol3/5m96_Mol3_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol3/
+
+mv Mol4/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol4/
+mv Mol4/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol4/
+mv Mol4/5m96_Mol4_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol4/
+mv Mol4/5m96_Mol4_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol4/
+
+mv Mol5/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol5/
+mv Mol5/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol5/
+mv Mol5/5m96_Mol5_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol5/
+mv Mol5/5m96_Mol5_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol5/
+
+mv Mol6/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol6/
+mv Mol6/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol6/
+mv Mol6/5m96_Mol6_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol6/
+mv Mol6/5m96_Mol6_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol6/
+
+mv Mol7/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol7/
+mv Mol7/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol7/
+mv Mol7/5m96_Mol7_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol7/
+mv Mol7/5m96_Mol7_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol7/
+
+mv Mol8/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol8/
+mv Mol8/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol8/
+mv Mol8/5m96_Mol8_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol8/
+mv Mol8/5m96_Mol8_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol8/
+
+mv Mol9/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol9/
+mv Mol9/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol9/
+mv Mol9/5m96_Mol9_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol9/
+mv Mol9/5m96_Mol9_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol9/
+
+mv Mol10/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol10/
+mv Mol10/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol10/
+mv Mol10/5m96_Mol10_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol10/
+mv Mol10/5m96_Mol10_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol10/
+
+mv Mol11/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol11/
+mv Mol11/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol11/
+mv Mol11/5m96_Mol11_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol11/
+mv Mol11/5m96_Mol11_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol11/
+
+mv Mol12/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol12/
+mv Mol12/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol12/
+mv Mol12/5m96_Mol12_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol12/
+mv Mol12/5m96_Mol12_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol12/
+
+mv Mol13/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol13/
+mv Mol13/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol13/
+mv Mol13/5m96_Mol13_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol13/
+mv Mol13/5m96_Mol13_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol13/
+
+mv Mol14/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol14/
+mv Mol14/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol14/
+mv Mol14/5m96_Mol14_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol14/
+mv Mol14/5m96_Mol14_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol14/
+
+mv Mol15/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol15/
+mv Mol15/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol15/
+mv Mol15/5m96_Mol15_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol15/
+mv Mol15/5m96_Mol15_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol15/
+
+mv Mol16/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol16/
+mv Mol16/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol16/
+mv Mol16/5m96_Mol16_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol16/
+mv Mol16/5m96_Mol16_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol16/
+
+mv Mol17/10.prod_4.rst7 /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol17/
+mv Mol17/10.prod_4.traj /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol17/
+mv Mol17/5m96_Mol17_solv.pdb /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol17/
+mv Mol17/5m96_Mol17_solv.prmtop /home/alonso/TFM/dockings/metabolites/5m96/moldinamics/5m96_Mol17/
+
+
+cd ..
+cd moldinamics
+
+cd 5m96_Mol1
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol1_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol1_final.pdb
+plip -f 5m96_Mol1_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol2
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol2_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol2_final.pdb
+plip -f 5m96_Mol2_final.pdb
+cpptraj -i rmsd.in  
+
+cd ..
+
+cd 5m96_Mol3
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol3_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol3_final.pdb
+plip -f 5m96_Mol3_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol4
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol4_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol4_final.pdb
+plip -f 5m96_Mol4_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol5
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol5_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol5_final.pdb
+plip -f 5m96_Mol5_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol6
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol6_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol6_final.pdb
+plip -f 5m96_Mol6_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol7
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol7_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol7_final.pdb
+plip -f 5m96_Mol7_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol8
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol8_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol8_final.pdb
+plip -f 5m96_Mol8_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol9
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol9_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol9_final.pdb
+plip -f 5m96_Mol9_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol10
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol10_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol10_final.pdb
+plip -f 5m96_Mol10_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol11
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol11_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol11_final.pdb
+plip -f 5m96_Mol11_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol12
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol12_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol12_final.pdb
+plip -f 5m96_Mol12_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol13
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol13_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol13_final.pdb
+plip -f 5m96_Mol13_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol14
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol14_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol14_final.pdb
+plip -f 5m96_Mol14_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol15
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol15_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol15_final.pdb
+plip -f 5m96_Mol15_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol16
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol16_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol16_final.pdb
+plip -f 5m96_Mol16_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+cd 5m96_Mol17
+cpptraj -i all_analysis.in
+ambpdb -p 5m96_Mol17_solv.prmtop -c 10.prod_4.rst7 > 5m96_Mol17_final.pdb
+plip -f 5m96_Mol17_final.pdb
+cpptraj -i rmsd.in 
+
+cd ..
+
+
+
+
+
+
+
+
+
